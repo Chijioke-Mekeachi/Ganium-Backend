3876ea33d2ea265b129c8e2c4012ef7d710262c3
chijiokemekeachi <chijiokemekelachi@gmail.com>
2026-08-25 19:36:57 +0100

    upadets the stmp port

--- src/utils/mail.go
+++ src/utils/mail.go
@@ -13,7 +13,7 @@ import (
 //   SMTP_PASS - Gmail App Password
 func SendEmail(to, subject, body string) error {
 	smtpHost := "smtp.gmail.com"
-	smtpPort := "587"
+	smtpPort := "465"
 
 	from := os.Getenv("SMTP_USER")
 	pass := os.Getenv("SMTP_PASS")
